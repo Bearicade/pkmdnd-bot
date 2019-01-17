5792b65806d82b29fc44ad740ba17f7e6ad6ecb4
pootdawg <38826285+pootdawg@users.noreply.github.com>
2019-01-17 16:08:28 -0500

    boot

--- command.js
+++ command.js
@@ -1,52 +1,73 @@
 var media = require("./media.json");
 var moves = require("./moves.json");
+var types = require("./types.json");
 const config = require("./config.json");
 
 module.exports = function () {
-  runCommand = function (prefix, client, msg, cmd, args) {
-    try {
-      //Try before throwing error
-      //Start comparing and analyzing commands
-      //Switch cases are temporary; implement only small processes
-      switch (cmd) {
+    runCommand = function (prefix, client, msg, cmd, args) {
+      try {
+        //Try before throwing error
+        //Start comparing and analyzing commands
+        //Switch cases are temporary; implement only small processes
+        switch (cmd) {
 
-        case 'list':  //lists the public commands
-        msg.channel.send("```These are my current commands:\n\n" +
-        "- ping: Pong!\n" +
-        "- hello: Say hi to me!\n" +
-        "- config: See your current settings.\n" +
-        "- roll [#][die]: I'll roll the specified die. (WIP)\n" +
-        "- move [move ID]: Check the move's properties (I only have 1-132)" + "```");
-        break;
+          case 'list':  //lists the public commands
+          msg.channel.send("```These are my current commands:\n\n" +
+          "- ping: Pong!\n" +
+          "- hello: Say hi to me!\n" +
+          "- config: See your current settings.\n" +
+          "- roll [#][die]: I'll roll the specified die. (WIP)\n" +
+          "- move [move ID]: Check the move's properties." + "```");
+          break;
 
-        case 'roll':  //roll random number within parameter
+          case 'roll':  //roll random number within parameter
 
-        break;
+          break;
 
-        //For fun, text responses
-        case 'ping':
-        msg.channel.send("Pong!");
-        break;
+          //For fun, text responses
+          case 'ping':
+          msg.channel.send("Pong!");
+          break;
 
-        case 'config':
-        msg.channel.send("```prefix: \"" + config.prefix + "\"\n" +
-        "server: \"" + config.server + "\"\n" + "channel: \"" + config.channel + "\"\n" + "```");
-        break;
+          case 'config':
+          msg.channel.send("```prefix: \"" + config.prefix + "\"\n" +
+          "server: \"" + config.server + "\"\n" + "channel: \"" + config.channel + "\"\n" + "```");
+          break;
 
-        case 'gutentag':
-        case 'hola':
-        case 'greetings':
-        case 'greet':
-        case 'hello': //takes both 'hi' and 'hello'
-        case 'hi':
-        msg.channel.send(randomFrom(media.greetings) + `*${msg.member}!*`);
+          case 'gutentag':
+          case 'hola':
+          case 'greetings':
+          case 'greet':
+          case 'hello': //takes both 'hi' and 'hello'
+          case 'hi':
+          msg.channel.send(randomFrom(media.greetings) + `*${msg.member}!*`);
+          break;
+
+          case 'type':
+
+          //switch (args[0])  { //display specific 'weak', 'ability' || 'minor/major'
+
+            //case 'weak':  //make sure to refer to #type.id
+            //case 'resist':
+            let list = "";
+            for (let i = 0; i < types[args[0]].weak.length; i++)  {
+              if (i != 0) {list += ", ";} //do not begin with comma on first run
+              list += types[args[0]].weak[i]; console.log(types[args[0]].weak[i])
+            }
+
+            msg.channel.send(list);
+
+            //break;
+
+            //default:  //on default, display everything of the type args[0]
+          //}
         break;
 
         case 'move':
         let move = "";
-          for (let i = 0; i < args.length; i++) {
-            move = move + args[i].toLowerCase();
-          }
+        for (let i = 0; i < args.length; i++) {
+          move = move + args[i].toLowerCase();
+        }
 
         msg.channel.send("```Move: " + moves[move].move + "\n" +
         "Category: " + moves[move].category + "\n" +
@@ -70,7 +91,7 @@ module.exports = function () {
 
   errorHandle = function (err, msg) {
     if (err.code === undefined) {
-      msg.channel.send("I am a work-in progress!");
+      msg.channel.send("I am a work-in-progress!");
     } else {
       msg.channel.send("Error: unhandled exception " + err.code);
     }
